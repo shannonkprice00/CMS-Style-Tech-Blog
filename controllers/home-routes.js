a0b88da085d1535d920cbd7ab001d120937cfa28
Shannon Price <124547956+shannonkprice00@users.noreply.github.com>
2023-05-22 16:34:12 -0500

    added routes, views, and code for debugging

---
+++ controllers/home-routes.js
@@ -0,0 +1,93 @@
+const router = require('express').Router();
+const { BlogPost, User } = require('../models');
+
+// GET all blogposts for homepage
+router.get('/', async (req, res) => {
+  try {
+    const dbBlogPostData = await BlogPost.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const blogPosts = dbBlogPostData.map((blogPost) =>
+      blogPost.get({ plain: true })
+    );
+
+    console.log(blogPosts);
+
+    res.render('homepage', {
+      blogPosts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// // GET one gallery
+// router.get('/gallery/:id', async (req, res) => {
+//   // If the user is not logged in, redirect the user to the login page
+//   if (!req.session.loggedIn) {
+//     res.redirect('/login');
+//   } else {
+//     // If the user is logged in, allow them to view the gallery
+//     try {
+//       const dbGalleryData = await Gallery.findByPk(req.params.id, {
+//         include: [
+//           {
+//             model: Painting,
+//             attributes: [
+//               'id',
+//               'title',
+//               'artist',
+//               'exhibition_date',
+//               'filename',
+//               'description',
+//             ],
+//           },
+//         ],
+//       });
+//       const gallery = dbGalleryData.get({ plain: true });
+//       res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
+//     } catch (err) {
+//       console.log(err);
+//       res.status(500).json(err);
+//     }
+//   }
+// });
+
+// // GET one painting
+// router.get('/painting/:id', async (req, res) => {
+//   // If the user is not logged in, redirect the user to the login page
+//   if (!req.session.loggedIn) {
+//     res.redirect('/login');
+//   } else {
+//     // If the user is logged in, allow them to view the painting
+//     try {
+//       const dbPaintingData = await Painting.findByPk(req.params.id);
+
+//       const painting = dbPaintingData.get({ plain: true });
+
+//       res.render('painting', { painting, loggedIn: req.session.loggedIn });
+//     } catch (err) {
+//       console.log(err);
+//       res.status(500).json(err);
+//     }
+//   }
+// });
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
